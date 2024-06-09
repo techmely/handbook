5abd9779b9dcc63e925107ad4538c8e3b506da7f
harrytran998 <nhattq.coding@gmail.com>
2024-06-09 13:48:07 +0700

    feat: add fontaine

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,22 @@
+import { FontaineTransform } from "fontaine";
 import createMDX from "fumadocs-mdx/config";
 
 const withMDX = createMDX();
 
+const fontTaineOptions = {
+  fallbacks: ["BlinkMacSystemFont", "Segoe UI", "Helvetica Neue", "Arial", "Noto Sans"],
+  resolvePath: (id) => `/public/fonts/${id}`,
+  overrideName: () => "Be VietNam Pro Techmely",
+};
+
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  webpack(config) {
+    config.plugins = config.plugins || [];
+    config.plugins.push(FontaineTransform.webpack(fontTaineOptions));
+    return config;
+  },
 };
 
 export default withMDX(config);
