42211ac790fdefc0162ce1825dd139bd642eb371
harrytran998 <nhattq.coding@gmail.com>
2024-06-11 22:49:37 +0700

    feat: add base seo metadata

---
+++ modules/seo/metadata.ts
@@ -0,0 +1,24 @@
+import { appConfig } from "@/app.config";
+import type { Metadata } from "next";
+
+export function createMetadata(override: Metadata): Metadata {
+  return {
+    ...override,
+    openGraph: {
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      url: appConfig.url,
+      images: "/thumbnail.png",
+      siteName: appConfig.name,
+      ...override.openGraph,
+    },
+    twitter: {
+      card: "summary_large_image",
+      creator: "@techmely",
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      images: "/thumbnail.png",
+      ...override.twitter,
+    },
+  };
+}
